6e54b63b0a121c207cc228524e7e097cbcae66ae
majima <majithabhanus@gmail.com>
2026-01-23 21:18:27 +0530

    Update script.sh

--- INSTANCE/script.sh
+++ INSTANCE/script.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
-  
-  set -eux
-exec > /var/log/user-data.log 2>&1
+set -eux
 
-# Wait for cloud-init to finish
-while [ ! -f /var/lib/cloud/instance/boot-finished ]; do
-  sleep 2
-done
+apt-get update -y
+apt-get install -y openjdk-21-jdk curl gnupg git
 
-apt update -y
-apt install -y openjdk-17-jre curl gnupg
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
-# Jenkins key
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key \
-  | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-
-# Jenkins repo
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-https://pkg.jenkins.io/debian binary/" \
+  https://pkg.jenkins.io/debian-stable binary/" \
   | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-apt update -y
-apt install -y jenkins
+apt-get update -y
+apt-get install -y jenkins
 
-systemctl daemon-reload
 systemctl enable jenkins
 systemctl start jenkins
