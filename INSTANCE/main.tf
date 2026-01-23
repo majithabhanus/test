707fb7812d110288d435ad8e624f571aeb52346a
majithabhanus <majithabhnaus@gmail.com>
2026-01-23 20:40:36 +0530

    222

--- INSTANCE/main.tf
+++ INSTANCE/main.tf
@@ -38,7 +38,7 @@ resource "aws_instance" "web" {
   user_data              = file("${path.module}/script.sh")
 
   tags = {
-    Name = "gpt clone"
+    Name = "gpt clone1"
   }
   root_block_device {
     volume_size = 30
