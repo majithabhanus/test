655a964a7044661a1dc683fffd13d2705286dd0a
majima <majithabhanus@gmail.com>
2026-01-24 19:42:20 +0530

    Update script.sh_recorrected

--- INSTANCE/script.sh
+++ INSTANCE/script.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-set -eux
+# Update and install Docker
+sudo apt update -y
+sudo apt install -y docker.io
 
-apt-get update -y
-apt-get install -y openjdk-21-jdk curl gnupg git
+# Start Docker and enable it
+sudo systemctl start docker
+sudo systemctl enable docker
 
-# Add new key
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+# Pull Jenkins LTS Docker image
+sudo docker pull jenkins/jenkins:lts
 
-# Add repo using the new key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list
-
-apt-get update -y
-apt-get install -y jenkins
-
-systemctl enable jenkins
-systemctl start jenkins
+# Run Jenkins container
+sudo docker run -d \
+  --name jenkins \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  -v jenkins_home:/var/jenkins_home \
+  jenkins/jenkins:lts
